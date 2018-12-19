554e574ebe268fd2246f456cc56b7e3d4c8bb1d8
Joshua Puello <joshua.puello@pragma.com.co>
2018-12-19 09:08:35 -0500

    Test module

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,10 +1,24 @@
 'use strict'
 
+const defaults = require('defaults')
 const setupDatabase = require('./lib/db')
 const setupAgentModel = require('./models/metric')
 const setupMetricModel = require('./models/agent')
+const setupAgent = require('./lib/agent')
 
 module.exports = async function (config) {
+  config = defaults(config, {
+    dialect: 'sqlite',
+    pool: {
+      max: 10,
+      min: 0,
+      idle: 10000
+    },
+    query: {
+      raw: true
+    }
+  })
+
   const sequelize = setupDatabase(config)
   const agentModel = setupAgentModel(config)
   const metricModel = setupMetricModel(config)
@@ -18,7 +32,7 @@ module.exports = async function (config) {
     await sequelize.sync({ force: true })
   }
 
-  const Agent = {}
+  const Agent = setupAgent(agentModel)
   const Metric = {}
 
   return {
